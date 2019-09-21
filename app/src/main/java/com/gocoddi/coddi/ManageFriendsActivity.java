4b4658ecbf3427d1776370d6c7fdc290eee43d5f
dylans123 <dylanskelly@knights.ucf.edu>
2019-09-20 23:19:05 -0400

    Add new manage friends panel for users to delete friends

--- app/src/main/java/com/gocoddi/coddi/ManageFriendsActivity.java
+++ app/src/main/java/com/gocoddi/coddi/ManageFriendsActivity.java
@@ -1,12 +1,47 @@
 package com.gocoddi.coddi;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ExpandableListAdapter;
+import android.widget.ExpandableListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class ManageFriendsActivity extends AppCompatActivity {
+
+    private static final String TAG = "Error";
+    private ExpandableListView expandableListView;
+    private ExpandableListAdapter expandableListAdapter;
+    private List<String> expandableListTitle;
+    private List options = new ArrayList();
+    private HashMap<String, List<String>> expandableListDetail;
+
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private FirebaseAuth auth = FirebaseAuth.getInstance();
+    private FirebaseUser user = auth.getCurrentUser();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +53,105 @@ public class ManageFriendsActivity extends AppCompatActivity {
 
         ActionBar ab = getSupportActionBar();
         ab.setDisplayHomeAsUpEnabled(true);
+
+        initFriends();
+    }
+
+    public void initFriends() {
+        options.add("remove");
+        expandableListDetail = new HashMap<>();
+
+        db.collection("users").document(user.getUid()).collection("friends").whereEqualTo("confirmed", true)
+            .get()
+            .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                    if (task.isSuccessful()) {
+                        if (task.getResult().isEmpty()) {
+                            ConstraintLayout layout = (ConstraintLayout) findViewById(R.id.layout);
+                            ConstraintLayout.LayoutParams lparams = new ConstraintLayout.LayoutParams(
+                                    ConstraintLayout.LayoutParams.WRAP_CONTENT, ConstraintLayout.LayoutParams.WRAP_CONTENT);
+                            lparams.setMargins(0, 0, 0, 0);
+                            TextView none = new TextView(ManageFriendsActivity.this);
+                            none.setText("You don't have any friends to manage.");
+                            none.setId(0);
+                            none.setLayoutParams(lparams);
+                            layout.addView(none);
+
+                            ConstraintSet set = new ConstraintSet();
+                            set.clone(layout);
+                            set.clear(R.id.friendText, ConstraintSet.BOTTOM);
+                            set.connect(none.getId(), ConstraintSet.TOP, R.id.friendText, ConstraintSet.BOTTOM, 100);
+                            set.connect(none.getId(), ConstraintSet.LEFT, ConstraintSet.PARENT_ID, ConstraintSet.LEFT, 0);
+                            set.connect(none.getId(), ConstraintSet.RIGHT, ConstraintSet.PARENT_ID, ConstraintSet.RIGHT, 0);
+                            set.applyTo(layout);
+                        } else {
+                            System.out.println("stuff was there");
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                String email = document.getString("email");
+                                expandableListDetail.put(email, options);
+                            }
+                        }
+                        expandableListView = (ExpandableListView) findViewById(R.id.expandableListView);
+                        System.out.println(expandableListDetail);
+                        expandableListTitle = new ArrayList<String>(expandableListDetail.keySet());
+                        expandableListAdapter = new CustomExpandableListAdapter(getApplicationContext(), expandableListTitle, expandableListDetail);
+                        expandableListView.setAdapter(expandableListAdapter);
+
+                        expandableListView.setOnGroupExpandListener(new ExpandableListView.OnGroupExpandListener() {
+
+                            @Override
+                            public void onGroupExpand(int groupPosition) {
+                            Toast.makeText(getApplicationContext(),
+                                expandableListTitle.get(groupPosition) + " List Expanded.",
+                                Toast.LENGTH_SHORT).show();
+                            }
+                        });
+
+                        expandableListView.setOnGroupCollapseListener(new ExpandableListView.OnGroupCollapseListener() {
+
+                            @Override
+                            public void onGroupCollapse(int groupPosition) {
+                            Toast.makeText(getApplicationContext(),
+                                expandableListTitle.get(groupPosition) + " List Collapsed.",
+                                Toast.LENGTH_SHORT).show();
+
+                            }
+                        });
+
+                        expandableListView.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+                            @Override
+                            public boolean onChildClick(ExpandableListView parent, View v,
+                                                        int groupPosition, int childPosition, long id) {
+                            db.collection("users").document(user.getUid()).collection("friends")
+                                    .whereEqualTo("email", expandableListTitle.get(groupPosition))
+                                    .limit(1)
+                                    .get()
+                                    .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                            if (task.isSuccessful()) {
+                                                for (QueryDocumentSnapshot document : task.getResult()) {
+                                                    db.collection("users")
+                                                        .document(user.getUid())
+                                                        .collection("friends")
+                                                        .document(document.getId())
+                                                        .delete();
+                                                }
+                                            }
+                                        }
+                                    });
+                                Intent i = new Intent(getApplicationContext(), HomeActivity.class);
+                                startActivity(i);
+                                finish();
+                                return false;
+                            }
+                        });
+                    } else {
+                        Log.d(TAG, "Error getting documents: ", task.getException());
+                    }
+                }
+            });
     }
 }
+
